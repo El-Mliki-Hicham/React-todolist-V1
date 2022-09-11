9d0f5e3078b1c2e371e48c4cc6bba566cf1f3d05
EL.MLIKI.HICHAM <92031524+El-Mliki-Hicham@users.noreply.github.com>
2022-09-11 19:47:08 +0200

    Delete function

--- src/App.js
+++ src/App.js
@@ -3,18 +3,24 @@ import Table from "./Components/table";
 import "bootstrap/dist/css/bootstrap.min.css"
 class App extends React.Component{
 
+  //State :data
   state={
     data:[
       {id:1,Nom:"hicham",Age:21},
       {id:2,Nom:'nada',Age:21}
     ]
   }
+//Delete function 
+  delete=(id)=>{
+    console.log(id)
+  }
+
   render(){
     return (
 
       <div>
-       
-        <Table Data={this.state.data} />
+      {/* send Data and Delete function to Table class */}
+        <Table Data={this.state.data} delete={this.delete} />
       </div>
     )
   }
--- src/Components/table.js
+++ src/Components/table.js
@@ -3,16 +3,16 @@ import React ,{ Fragment } from "react";
 class Table extends React.Component{
     render(){
         const Value = this.props.Data ;
-        
+        const  Delete =this.props.delete
         const Show_Value = Value.map(value => {
             
             return(
-            <Fragment>
+            <Fragment key={value.id}>
                 <tr >
                    <td >{value.id}</td>
                    <td>{value.Nom}</td>
                    <td>{value.Age}</td>
-                   <td><button type="button" className="btn btn-dark">X</button></td>
+                   <td><button onClick={ ()=> Delete(value.id)} type="button" className="btn btn-dark">X</button></td>
                 </tr>
                
             </Fragment>
@@ -23,7 +23,7 @@ class Table extends React.Component{
             return(
                 <div >
 
-                    <table className="table">
+                    <table className="table" >
                     <thead>
                         <tr>
                             <th>Id</th>
